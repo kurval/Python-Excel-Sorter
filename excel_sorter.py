b660bb074195dd553f0c8f42e8a5b07c9f91cfef
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-05-04 11:52:02 +0300

    fixed ascending problem and adding options func

--- excel_sorter.py
+++ excel_sorter.py
@@ -2,6 +2,16 @@
 import pandas as pd
 pd.set_option('display.max_columns', None)
 
+def options(cols, opt):
+    x = 0
+    print("\nOptions:\n")
+    for col in cols:
+        if opt:
+            print(str(x) + ": " + col)
+        else:
+            print(col)
+        x += 1
+
 # Source Info
 form = int(input("Do you want to open csv or excel file? Enter '1' for CSV or '2' for excel: "))
 file1 = input("Enter location/name of file: ")
@@ -18,24 +28,21 @@ sorter = []
 style = []
 sort = True
 while sort:
-    sorter.append(input(("Name of the column you wish to sort by: ")))
-    style.append(input(("Type 'True' for Ascending, 'False' for Descending sort: ")).capitalize())
+    options(cols, 0)
+    sorter.append(input(("\nName of the column you wish to sort by: ")))
+    style.append(int(input("Type '1' for Ascending, '0' for Descending sort: ")))
     sort = input("Do you want to use another column for sorting? Enter 'y' for yes and 'n' no: ")
     if sort[0] == 'n':
         sort = False
     else:
         continue
-new_df = df.sort_values(sorter,ascending=style)
+new_df = df.sort_values(sorter, ascending=style)
 
 # Filter Info
 filter_col = input("Would you like to filter columns? Enter 'y' for yes and 'n' for no: ")
 if filter_col[0] == 'y':
-    x = 0
-    print("\nOptions:\n")
-    for col in cols:
-        print(str(x) + ": " + col)
-        x += 1
-
+    
+    options(cols, 1)
     columns = print("\nChoose range of columns to show")
     from_col = int(input("Enter number of the first column to show: "))
     to_col = int(input("Enter number of the last column to show: "))
